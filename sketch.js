4b385df4efcdc7e050bd1ac0bb8813a005f38ce2
Hosain-J <mhosainj2020@gmail.com>
2025-04-22 15:07:23 -0600

    April 22, 2025
    the background has been fixed. now the background color blends with the background image

--- sketch.js
+++ sketch.js
@@ -4,18 +4,35 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let lvl=1;
-let lvl1;
 
-function preload(){
-    lvl1 = loadImage("assets (final draft)/lvl 1.png");
+let lvl1;
+let xOffset, yOffset;
+
+function preload() {
+  lvl1 = loadImage("assets (final draft)/lvl 1.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  image(lvl1,0,0,width,height);
+  calculateOffsets();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  calculateOffsets();
+}
+
+function calculateOffsets() {
+  // center the image in the canvas
+  xOffset = (windowWidth  - lvl1.width)  / 2;
+  yOffset = (windowHeight - lvl1.height) / 2;
 }
 
 function draw() {
-  //background(220);
+  background(62, 56, 80, 255);
+
+  image(lvl1, xOffset, yOffset);
+  
 }
+
+
